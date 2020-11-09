3ebd9779f9ca9d01d00e5dd3f3a0d244d6a69510
Carlos <c.mata1589@gmail.com>
2020-11-09 21:12:51 +0100

    init

---
+++ task2/index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+$a = readline('Enter your Name: ');
+
+//
+// For output
+ if ($a == "Alice" || $a =="Bob") {
+    echo "Hello " . $a . ", Welcome.";
+  } else {
+      echo "You are not Welcome";
+  }
+ ?>
---
+++ task3/index.php
@@ -0,0 +1,27 @@
+<?php
+///task 4
+// $a = readline('Enter a number: ');
+// $i = 1;
+// $sum = 0;
+// while ($i <= $a) {
+//   $sum = $i + $sum;
+//   $i = $i + 1;
+//
+// }
+// echo $sum
+
+
+//task 5
+$a = readline('Enter a number: ');
+$i = 1;
+$sum = 0;
+while ($i <= $a) {
+  if ($i % 3 == 0 || $i % 5 == 0) {
+    $sum = $i + $sum;
+  }
+  $i = $i + 1;
+}
+echo $sum
+
+
+ ?>
