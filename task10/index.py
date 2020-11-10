ee203ba74ce2e3e9c86cebd99f807b1e652a6b45
Carlos <c.mata1589@gmail.com>
2020-11-10 20:38:55 +0100

    task10

---
+++ task10/index.py
@@ -0,0 +1,15 @@
+import cmath
+
+x = 10
+y = 2
+xy = y
+while xy < x:
+    xy = xy*y
+    print (xy)
+
+if xy == x:
+    print("true")
+else:
+    print("False")
+
+
--- task9/index.php
+++ task9/index.php
@@ -1,17 +1,26 @@
 <?php
-$dt = new DateTime();
-$year = $dt->format('Y');
+// $dt = new DateTime();
+// $year = $dt->format('Y');
+$year = "2021";
 $i = 0;
 
-$a = date('L');
-if ($a == 1) {
-  echo $year;
-  while ($i <= 20) {
-    $year = $year + 4;
+$a = date('L',strtotime("$year-01-01"));
+echo $a;
+
+if ($a = 0) {
+  $b= 0
+  while ($b = 0) {
+    $year = $year + 1;
+    $b= date('L',strtotime("$year-01-01"));
     echo $year;
-    $i++;
   }
-}
-// falta programar el caso que el ano presente no sea Leap
+} if ($a = 1 ) {
+    echo $year;
+    while ($i <= 20) {
+      $year = $year + 4;
+      echo $year ;
+      $i++;
+    }
+  }
 
  ?>
